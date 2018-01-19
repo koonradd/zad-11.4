7d0700ca06862919fdcaed3073677415894356f7
Konrad Kościelski <konrad.koscielski@interia.pl>
2018-01-19 17:02:53 +0100

    Add

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
-function Phone(brand, price, color) {
+function Phone(brand, price, color, display) {
 	this.brand = brand;
-	//by using this, the "brand" property of the object we create will assume the value of the brand argument
 	this.price = price;
 	this.color = color;
+	this.display = display;
 }
 Phone.prototype.printInfo = function() {
-		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+		console.log("The phone brand is " + this.brand + ", color is " + this.color + ", price is " + this.price + " and the display is " + this.display + ".");
 }
-var iPhone6S = new Phone("Apple", 2250, "silver");
-var SamsungGalaxyS6 = new Phone ("Samsung", 2000, "black");
-var OnePlusOne = new Phone ("One Plus", 1500, "red");
+var iPhone6S = new Phone("Apple", 2250, "silver", "IPS");
+var SamsungGalaxyS6 = new Phone ("Samsung", 2000, "black", "OLED");
+var OnePlusOne = new Phone ("One Plus", 1500, "red", "IPS");
 iPhone6S.printInfo();
 SamsungGalaxyS6.printInfo();
 OnePlusOne.printInfo();
